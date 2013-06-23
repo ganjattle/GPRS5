4a59250dafb38d8924a7d8ce7dea3261b2124eb5
ganyaotong <ganjattle@gmail.com>
2013-06-24 02:34:18 +0800

    ksfjl

--- js/application.js
+++ js/application.js
@@ -1,9 +1,45 @@
 // JavaScript Document
 document.addEventListener( "deviceready", onDeviceReady );
 
-var sfw;
+var templates = {
+    home:"views/ad.html",
+    loaded: 0,
+    requested: 0,
+}; 
+
 
 function onDeviceReady( event ) {
+	alert("jasflk");
   console.log("deviceready");
-  sfw = new SalesforceWrapper();
+  //load Mousetache HTML templates
+    for (var key in templates) {
+        (function() {
+            var _key = key.toString();
+            if ( _key != "loaded" && _key != "requested" ){
+                templates.requested ++;
+         
+                 var templateLoaded = function( template ){
+                    onTemplateLoaded( template, _key );
+                 }
+                
+                $.get( templates[ _key ], templateLoaded );
+             }
+         })();
+    }
+}
+function onTemplateLoaded(template, key) {
+    
+   // console.log( key + ": " + template);
+    templates[ key ] = template;
+    templates.loaded ++;
+    
+    if ( templates.loaded == templates.requested ) {
+        setupDefaultView();
+    }
+}
+var header, container;
+
+function setupDefaultView() {
+    //console.log("setupDefaultView");
+    $("body").html( templates.home );
 }
